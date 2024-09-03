9aaaf74371785ac4cdd5ccf55eaa0455fd6c282f
codepotid <dicky.wahyu376@guru.smk.belajar.id>
2024-09-03 12:19:28 +0700

    firt commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman Contact</title>
+</head>
+<body>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/blog">Blog</a>
+    <a href="/contact">Contact</a>
+    <h1>Halaman Contact</h1>
+    <ul>
+        <li>
+            <a href="https://instagram.com/pottttttttt86" target="_blank" rel="noopener noreferrer">Instagram</a>
+        </li>
+        <li>
+            <a href="https://x.com/pottttttttt86" target="_blank" rel="noopener noreferrer">X</a>
+        </li>
+        <li>
+            <a href="https://youtube.com/@PONDOKDENANYAR" target="_blank" rel="noopener noreferrer">YouTube</a>
+        </li>
+    </ul>
+</body>
+</html>
