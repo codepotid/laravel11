879758b99b15542802ca2801ef39253ea6a0b83c
codepotid <dicky.wahyu376@guru.smk.belajar.id>
2024-09-20 19:16:46 +0700

    last commit

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,38 +1,12 @@
-<?php 
+<?php
+
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class post
+class Post extends Model
 {
-    public static function all ()
-    {
-        return [
-            [
-                'id' => 1,
-                'slug' => 'judul-artikel-1',
-                'title' => 'Judul Artikel 1',
-                'author' => 'Dicky Wahyu Pratama',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Id expedita laborum, reprehenderit ducimus consectetur molestiae ad maxime, optio asperiores, ratione earum cum odio doloremque voluptatum dignissimos error quia minima exercitationem?'
-            ],
-            [
-                'id' => 2,
-                'slug' => 'judul-artikel-2',
-                'title' => 'Judul Artikel 2',
-                'author' => 'Dicky Wahyu Pratama',
-                'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos eum dolorem illum. Libero, hic repudiandae, dolorem nemo dolor molestias aperiam reiciendis, vitae ad in quisquam minima fugiat debitis nesciunt esse.'
-            ]
-            ];
-    }
-
-    public static function find($slug): array
-    {
-        $post= Arr::first(static::all(), fn ($post) => $post['slug'] == $slug);
-
-        if (! $post) {
-            abort(404);
-        }
-
-        return $post;
-    }
+    use HasFactory;
+    protected $fillable = ['title', 'author', 'slug', 'body'];
 }
